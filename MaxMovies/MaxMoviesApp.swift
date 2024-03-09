b4f29c8f427c705d384cb05bfd25fb211338820b
mxzmr <mxzmr@proton.me>
2024-03-09 07:58:13 +0200

    Initial Commit

---
+++ MaxMovies/MaxMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxMoviesApp.swift
+//  MaxMovies
+//
+//  Created by Max zam on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaxMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
